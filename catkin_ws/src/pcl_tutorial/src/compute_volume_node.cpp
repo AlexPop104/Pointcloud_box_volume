536f3886c7c12c07ffcc041e6a213932bd08e0d8
AlexPop104 <alexandrupop104@gmail.com>
2020-11-24 10:53:18 +0200

    Pe moment nu merge rqt. Incerc sa repar

--- catkin_ws/src/pcl_tutorial/src/compute_volume_node.cpp
+++ catkin_ws/src/pcl_tutorial/src/compute_volume_node.cpp
@@ -48,23 +48,52 @@ public:
 
     float Coeficients[3][4];
 
-    float Volum=1;
+    float Volum = 1;
 
     pub1_ = nh_.advertise<sensor_msgs::PointCloud2>("/output_plan", 1);
     pub2_ = nh_.advertise<sensor_msgs::PointCloud2>("/output_proiectii", 1);
+    pub3_ = nh_.advertise<sensor_msgs::PointCloud2>("/cloud_floor", 1);
     sub_ = nh_.subscribe("/pf_out", 1, &ComputeVolumeNode::cloudCallback, this);
-    //config_server_.setCallback(boost::bind(&ComputeVolumeNode::dynReconfCallback, this, _1, _2));
-
-    vis_pub = nh_.advertise<visualization_msgs::Marker>( "/Volum_final", 0 );
-
     
+
+    vis_pub = nh_.advertise<visualization_msgs::Marker>("/Volum_final", 0);
+    vis2_pub = nh_.advertise<visualization_msgs::Marker>("/Nr_of_planes", 0);
   }
 
   ~ComputeVolumeNode() {}
 
-  void euclidean_segmenting(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f, bool &ok2)
+  void planar_segmenting_single_time(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_segmented,pcl::ModelCoefficients::Ptr coefficients)
   {
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+    
+    
+    pcl::PointCloud<pcl::PointXYZ>::Ptr outliers(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr outliers_segmented(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::SACSegmentation<pcl::PointXYZ> seg;
 
+    // Optional
+    seg.setOptimizeCoefficients(true);
+    // Mandatory
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setDistanceThreshold(0.01);
+
+    // Segment dominant plane
+    seg.setInputCloud(cloud);
+    seg.segment(*inliers, *coefficients);
+    pcl::copyPointCloud<pcl::PointXYZ>(*cloud, *inliers, *cloud_segmented);
+
+    if (inliers->indices.size() == 0)
+    {
+
+      PCL_ERROR("Could not estimate a planar model for the given dataset.");
+      ok2 = 0;
+    }
+
+  }
+
+  void euclidean_segmenting(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f, bool &ok2)
+  {
 
     // Create the filtering object: downsample the dataset using a leaf size of 1cm
     pcl::VoxelGrid<pcl::PointXYZ> vg;
@@ -74,9 +103,6 @@ public:
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZ>());
-    
-
-
 
     vg.setInputCloud(cloud);
     vg.setLeafSize(0.01f, 0.01f, 0.01f);
@@ -156,11 +182,11 @@ public:
       ok2 = 0;
     }
   }
-
-  void planar_segmenting(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,float Coeficients[3][4], pcl::PointCloud<pcl::PointXYZ>::Ptr all_planes[4],pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final , int t, bool &ok2)
+  
+  void planar_segmenting(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, float Coeficients[3][4], pcl::PointCloud<pcl::PointXYZ>::Ptr all_planes[4], pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final, int t, bool &ok2)
   {
-     pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-      pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_segmented(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr outliers(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr outliers_segmented(new pcl::PointCloud<pcl::PointXYZ>);
@@ -201,7 +227,7 @@ public:
     }
   }
 
-  void create_lines(float Coeficients[3][4], pcl::PointCloud<pcl::PointXYZ>::Ptr all_planes[4],pcl::PointCloud<pcl::PointXYZ> all_lines[4][4],pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_linii,bool &ok2)
+  void create_lines(float Coeficients[3][4], pcl::PointCloud<pcl::PointXYZ>::Ptr all_planes[4], pcl::PointCloud<pcl::PointXYZ> all_lines[4][4], pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_linii, bool &ok2)
   {
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
@@ -265,7 +291,7 @@ public:
     }
   }
 
-  void project_line_2_plane(float Coeficients[3][4], pcl::PointCloud<pcl::PointXYZ>::Ptr all_planes[4],pcl::PointCloud<pcl::PointXYZ> all_lines[4][4],pcl::PointCloud<pcl::PointXYZ>::Ptr all_projected_lines[4][4],pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_proiectii)
+  void project_line_2_plane(float Coeficients[3][4], pcl::PointCloud<pcl::PointXYZ>::Ptr all_planes[4], pcl::PointCloud<pcl::PointXYZ> all_lines[4][4], pcl::PointCloud<pcl::PointXYZ>::Ptr all_projected_lines[4][4], pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_proiectii)
   {
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
@@ -311,7 +337,6 @@ public:
             j = aux2;
 
             pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected(new pcl::PointCloud<pcl::PointXYZ>);
-           
 
             *cloud = all_lines[i][j];
 
@@ -334,7 +359,7 @@ public:
     }
   }
 
-  void compute_volume(pcl::PointCloud<pcl::PointXYZ>::Ptr all_projected_lines[4][4],float &Volum)
+  void compute_volume(pcl::PointCloud<pcl::PointXYZ>::Ptr all_projected_lines[4][4], float &Volum)
   {
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
@@ -485,55 +510,76 @@ public:
               << "\n";
   }
 
-void compute_all(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final,pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_proiectii,pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_linii,float &Volum , int &t)
+  void compute_all(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_floor, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_proiectii, pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_linii, float &Volum, int &p)
   {
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f(new pcl::PointCloud<pcl::PointXYZ>);
-   
+
     float Coeficients[3][4];
-  
+
     pcl::PointCloud<pcl::PointXYZ> all_lines[4][4];
     pcl::PointCloud<pcl::PointXYZ>::Ptr all_planes[4];
     pcl::PointCloud<pcl::PointXYZ>::Ptr all_projected_lines[4][4];
-  
 
+    pcl::ModelCoefficients::Ptr coefficients_floor(new pcl::ModelCoefficients);
 
-    bool ok=1;
+    
+
+    bool ok = 1;
 
     bool ok2;
 
     pcl::PCDWriter writer;
 
-    if (cloud->size()==0){
-      ok=0;
-      t=0;
-    }
-
-    for (t = 1; (t < 4) && ok; t++)
+    if (cloud->size() == 0)
     {
-      
-      
-      euclidean_segmenting(cloud,cloud_f,ok2);
-
-      if (cloud_f->size()==0){
-      ok=0;
-      }
-
-      if (ok2!=0) {
-      planar_segmenting(cloud_f,Coeficients,all_planes,cloud_final, t, ok2);  //cloud_f is global, so the modifications stay
-
-      cloud = cloud_f;  // Cloud is now the extracted pointcloud
-
-      if (cloud->size()==0){
-      ok=0;
-      }
-      }
-
-      
+      ok = 0;
+      p = 0; // NO PLANE
     }
 
-    if (ok && ok2){
- 
+    planar_segmenting_single_time(cloud,cloud_floor,coefficients_floor);
+
+
+
+    Eigen::Vector3f normal_floor;
+    normal_floor << coefficients_floor->values[0], coefficients_floor->values[1], coefficients_floor->values[2];
+    if(coefficients_floor->values[3] < 0)
+    {
+      normal_floor *= -1;
+    }
+
+    for (int t = 1; (t < 4) && ok; t++)
+    {
+
+      euclidean_segmenting(cloud, cloud_f, ok2);
+
+      if (cloud_f->size() == 0)
+      {
+        ok = 0; //No more planes to cut out
+      }
+
+      if (ok2 != 0)
+      {
+        planar_segmenting(cloud_f, Coeficients, all_planes, cloud_final, t, ok2); //cloud_f is global, so the modifications stay
+
+        cloud = cloud_f; // Cloud is now the extracted pointcloud
+
+        if (cloud->size() == 0)
+        {
+          ok = 0;
+          if (t == 1)
+          {
+            p = 1; // Only the Ground Plane
+          }
+        }
+
+        p = t + 1;
+      }
+    }
+
+    if (ok && ok2)
+    {
+
       /*     
       std::stringstream ss2, ss3, ss4;
      
@@ -544,7 +590,7 @@ void compute_all(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,pcl::PointCloud<pcl::
 
       */
 
-      create_lines(Coeficients,all_planes,all_lines,cloud_linii,ok2);
+      create_lines(Coeficients, all_planes, all_lines, cloud_linii, ok2);
 
       /*
       ss3 << "All_lines"
@@ -553,45 +599,42 @@ void compute_all(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,pcl::PointCloud<pcl::
       writer.write<pcl::PointXYZ>(ss3.str(), *cloud_linii, false);
       */
 
-      project_line_2_plane(Coeficients,all_planes,all_lines,all_projected_lines,cloud_proiectii);
+      project_line_2_plane(Coeficients, all_planes, all_lines, all_projected_lines, cloud_proiectii);
 
-         /*
+      /*
        ss4 << "All_projections"
         << ".pcd";
 
        writer.write<pcl::PointXYZ>(ss4.str(), *cloud_proiectii, false);
                   */
 
-
-      compute_volume(all_projected_lines,Volum);
+      compute_volume(all_projected_lines, Volum);
     }
-    else {
+    else
+    {
       /*std::cout<<"Not enough planes";*/
     }
-
-    
-   
   }
 
-
-  /*
+  
   void
-  dynReconfCallback(pcl_tutorial::compute_volume_nodeConfig &config, uint32_t level)
+  dynReconfCallback()
   {
 
   }
-  */
+  
 
   void
   cloudCallback(const sensor_msgs::PointCloud2ConstPtr &cloud_msg)
   {
-    float Volum=1;
-    int t=0;
-
+    float Volum = 1;
+    int p = 0;
 
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_proiectii(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_linii(new pcl::PointCloud<pcl::PointXYZ>);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_floor(new pcl::PointCloud<pcl::PointXYZ>);
 
     pcl::PointCloud<pcl::PointXYZ> cloud_Test;
     pcl::fromROSMsg(*cloud_msg, cloud_Test);
@@ -599,128 +642,165 @@ void compute_all(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,pcl::PointCloud<pcl::
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloudPTR(new pcl::PointCloud<pcl::PointXYZ>);
     *cloudPTR = cloud_Test;
 
-    compute_all(cloudPTR,cloud_final,cloud_proiectii,cloud_linii,Volum,t);
+    compute_all(cloudPTR,cloud_floor, cloud_final, cloud_proiectii, cloud_linii, Volum, p);
+
+    
 
     sensor_msgs::PointCloud2 tempROSMsg;
     sensor_msgs::PointCloud2 tempROSMsg2;
-
-  
-
+     sensor_msgs::PointCloud2 tempROSMsg3;
 
     pcl::toROSMsg(*cloud_final, tempROSMsg);
     pcl::toROSMsg(*cloud_proiectii, tempROSMsg2);
-
-
-    
+    pcl::toROSMsg(*cloud_floor, tempROSMsg3);
 
     tempROSMsg.header.frame_id = "camera_depth_optical_frame";
     tempROSMsg2.header.frame_id = "camera_depth_optical_frame";
+    tempROSMsg3.header.frame_id = "camera_depth_optical_frame";
+   
+    //Message Marker Volume
+    ////////////////////////////////////////    
+    std::stringstream ss;
+
+    ss << "Volumul este " << Volum << " m3";
+
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "camera_depth_optical_frame";
+    marker.header.stamp = ros::Time::now();
+    marker.pose.position.x = 1;
+    marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+    marker.text = ss.str();
+    marker.action = visualization_msgs::Marker::ADD;
+
+    marker.pose.position.y = 1;
+    marker.pose.position.z = 1;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+    marker.scale.x = 1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
+    marker.color.a = 1.0; // Don't forget to set the alpha! Otherwise it is invisible
+    marker.color.r = 0.0;
+    marker.color.g = 1.0;
+    marker.color.b = 0.0;
+    marker.lifetime = ros::Duration();
+
+    //////////////////////////////////////////////////////////////////////////////////
+
+     //Message Marker Volume
+    ////////////////////////////////////////    
+    std::stringstream ss2;
+
+    switch (p)
+    {
+    case 0:
+      ss2 << "No plane detected"
+                << "\n";
+      break;
+    case 1:
+      ss2  << "Only ground plane detected"
+                << "\n";
+      break;
+    case 2:
+      ss2  << "Ground plane and 1 plane detected"
+                << "\n";
+      break;
+    case 3:
+      ss2  << "Ground plane and 2 planes detected"
+                << "\n";
+      break;
+    case 4:
+      ss2  << "Ground plane and 3 planes detected"
+                << "\n";
+      break;
+    }
 
     
 
+    visualization_msgs::Marker marker2;
+    marker2.header.frame_id = "camera_depth_optical_frame";
+    marker2.header.stamp = ros::Time::now();
+    marker2.pose.position.x = 1;
+    marker2.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+    marker2.text = ss2.str();
+    marker2.action = visualization_msgs::Marker::ADD;
 
-      //Vizualizare Volum
-
-      std::stringstream ss;
-
-       ss<<"Volumul este "<<Volum<<" m3";
-
-
-
-      visualization_msgs::Marker marker;  
-       marker.header.frame_id = "camera_depth_optical_frame";
-      marker.header.stamp = ros::Time::now();
-      marker.pose.position.x = 1;
-      marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-      marker.text=ss.str();
-      marker.action = visualization_msgs::Marker::ADD;
-
-      marker.pose.position.y = 1;
-      marker.pose.position.z = 1;
-      marker.pose.orientation.x = 0.0;
-      marker.pose.orientation.y = 0.0;
-      marker.pose.orientation.z = 0.0;
-      marker.pose.orientation.w = 1.0;
-      marker.scale.x = 1;
-      marker.scale.y = 0.1;
-      marker.scale.z = 0.1;
-      marker.color.a = 1.0; // Don't forget to set the alpha! Otherwise it is invisible
-      marker.color.r = 0.0;
-      marker.color.g = 1.0;
-      marker.color.b = 0.0;
-      marker.lifetime = ros::Duration();
-
+    marker2.pose.position.y = 1;
+    marker2.pose.position.z = 1;
+    marker2.pose.orientation.x = 0.0;
+    marker2.pose.orientation.y = 0.0;
+    marker2.pose.orientation.z = 0.0;
+    marker2.pose.orientation.w = 1.0;
+    marker2.scale.x = 1;
+    marker2.scale.y = 0.1;
+    marker2.scale.z = 0.1;
+    marker2.color.a = 1.0; // Don't forget to set the alpha! Otherwise it is invisible
+    marker2.color.r = 0.0;
+    marker2.color.g = 1.0;
+    marker2.color.b = 0.0;
+    marker2.lifetime = ros::Duration();
 
     //////////////////////////////////////////////////////////////////////////////////
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-  
-
- 
-
     //Publish the data
-          
+
     pub1_.publish(tempROSMsg);
     pub2_.publish(tempROSMsg2);
-    vis_pub.publish( marker);
+    pub3_.publish(tempROSMsg3);
+
+
+    vis_pub.publish(marker);
+    vis2_pub.publish(marker2);
 
     cloud_final->clear();
     cloud_linii->clear();
     cloud_proiectii->clear();
 
-    Volum=1;
+    Volum = 1;
   }
 
 private:
+  bool ok2;
 
-bool ok2;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered;
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud;
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_f;
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_linii;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_proiectii;
 
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  pcl::PointIndices::Ptr inliers;
+  pcl::ModelCoefficients::Ptr coefficients;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane;
 
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_final;
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_linii;
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_proiectii;
-
-pcl::SACSegmentation<pcl::PointXYZ> seg;
-pcl::PointIndices::Ptr inliers;
-pcl::ModelCoefficients::Ptr coefficients;
-pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane;
-
- pcl::PointCloud<pcl::PointXYZ> all_lines[4][4];
+  pcl::PointCloud<pcl::PointXYZ> all_lines[4][4];
   pcl::PointCloud<pcl::PointXYZ>::Ptr all_planes[4];
   pcl::PointCloud<pcl::PointXYZ>::Ptr all_projected_lines[4][4];
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_floor;
 
   float Coeficients[3][4];
 
   float Volum;
 
-
-
-
   ros::NodeHandle nh_;
   ros::Subscriber sub_;
   ros::Publisher pub1_;
   ros::Publisher pub2_;
+  ros::Publisher pub3_;
 
   ros::Publisher vis_pub;
+  ros::Publisher vis2_pub;
   //dynamic_reconfigure::Server<pcl_tutorial::compute_volume_nodeConfig> config_server_;
 
-  //ros::Publisher vis_pub = node_handle.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
+  
 };
 
 int main(int argc, char **argv)
